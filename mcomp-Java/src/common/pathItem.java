bf5a2b21906841ca2ad6d8de4b867060348563db
David Avery <David Avery@XPS15>
2017-12-06 09:15:49 +0000

    pathitem (waypoint) holder f0r queue

---
+++ mcomp-Java/src/common/pathItem.java
@@ -0,0 +1,26 @@
+package common;
+
+public class PathItem {
+    
+    private Waypoint data;
+    private PathItem next;
+    
+    public PathItem(Waypoint data) {
+        this.data = data;
+    }
+    public Waypoint getData() {
+        return data;
+    }
+    public PathItem getNext() {
+        return next;
+    }
+    public void setNext(PathItem next) {
+        this.next = next;
+    }
+    
+    public String toString() {
+        return String.format("[%s,%d]", data.toString());
+    }
+
+}
+
