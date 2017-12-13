dc7358bf94be1e386f77fc4c70e181a601ea31ab
Stephen Pope <stevempope@gmail.com>
2017-12-13 11:48:42 +0000

    Herd member relationship (#19)
    * Herd and Member Base Classes
    
    * Added jUnit and refined Herd Data Types
    
    * Added Unit Testing
    
    * Added getters for Herd Lists, changed DestSetter to a Member
    
    * Added ManyMember test, added list population to requestJoin
    
    * Refined requestJoin to maintain single Dest Setter
    
    * Added TODO to manyMembers jUnit test

---
+++ mcomp-Java/src/common/Herd.java
@@ -0,0 +1,220 @@
+package common;
+
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author Stephen Pope 15836791
+ *
+ *	A test class for a Herd.
+ *
+ *	In order to exist, a Herd must have 1 member when it is created.
+ *	Once created, a Herd must nominate a Leader. This will always 
+ *	result in the Founding member beginning as the leader.
+ *
+ *	A Herd holds lists containing all members and lists for members with
+ *	certain abilities.
+ *	
+ */
+
+public class Herd {
+	private String herdID;
+	private Member theLeader;
+	private ArrayList <Member> herdMembers;
+	private ArrayList <Member> herdDrivers;
+	private ArrayList <Member> herdSensors;
+	private ArrayList <Member> herdProcessors;
+	private ArrayList <Member> herdViewers;
+	private Member herdDestSetters;
+
+	/*
+	 * The Herd constructor.
+	 * 
+	 * A new Herd is handed its founding member.
+	 * When storage has been allocated, the founding
+	 * member is queried for its abilities. It will
+	 * then populate the appropriate lists. An election
+	 * is then called.
+	 */
+	public Herd (Member a) {
+		//Storage Initialisation
+		herdID = "newHerd"; //Needs to be a randomly generated name
+		herdMembers = new ArrayList<Member>();
+		herdDrivers = new ArrayList<Member>();
+		herdSensors = new ArrayList<Member>();
+		herdProcessors = new ArrayList<Member>();
+		herdViewers = new ArrayList<Member>();
+
+		//Ability Querying
+		herdMembers.add(a);
+		for(String b : a.getAbilities()) {
+			switch (b) {
+			case "D":
+				herdDrivers.add(a);
+				break;
+			case "P":
+				herdProcessors.add(a);
+				break;
+			case "S":
+				herdSensors.add(a);
+				break;
+			case "V":
+				herdViewers.add(a);
+				break;
+			case "W":
+				herdDestSetters = a;
+				break;
+			}
+		}
+		//election
+		theLeader = a;
+	}
+	
+	/*
+	 * The nomination process. The Leader variable is populated
+	 * by the Member Object of the Leader.
+	 */
+	public Member nominateLeader() {
+		//Obviously the data type and nomination process needs refining!
+		theLeader = herdMembers.get(0);
+		return theLeader;
+	}
+	
+	
+	/*
+	 * This is where requests to join the Herd are handled. In the
+	 * full implementation, a Herd can only be "merged" with another,
+	 * so this method will either change signature or be deprecated.
+	 * 
+	 * A member requests to join the Herd by calling this method. The
+	 * Herd will request a Public Key and Abilities List. If the Public
+	 * Key is a valid Public Key (Maybe send a file and see if it returns
+	 * the correct answer?) Then the Herd will respond with the herdMembers
+	 * List. Spreading the List like this should prevent Herd collapse in
+	 * the event that the Leader dies/disappears.
+	 */
+	public ArrayList<Member> requestJoin(Member aspiringMember){
+		//TODO Find a test to validate the Key of a member
+		//TODO Populate lists based on ability
+		herdMembers.add(aspiringMember);
+		for(String ability: aspiringMember.getAbilities()) {
+			switch (ability) {
+			case "D":
+				herdDrivers.add(aspiringMember);
+				break;
+			case "P":
+				herdProcessors.add(aspiringMember);
+				break;
+			case "S":
+				herdSensors.add(aspiringMember);
+				break;
+			case "V":
+				herdViewers.add(aspiringMember);
+				break;
+			case "W":
+				//TODO Need code to check the old destSetter and remove it if it only has the one ability.
+				herdDestSetters.getAbilities().remove("W");
+				herdDestSetters.leaveHerd(this);
+				herdDestSetters = aspiringMember;
+				break;
+			}
+		}
+		return herdMembers;
+	}
+	
+	/*
+	 * This where requests to leave the Herd are handled. In order to leave,
+	 * a member must removed from the Herds lists of specialists. All remaining
+	 * members must be notified of the new state of the Herd. If the leaving member
+	 * is a Leader, then an election must be held.
+	 * 
+	 * A debate needs to be about transferring leadership, and what gets copied over.
+	 */
+	public ArrayList<Member> requestLeave(Member leavingMember){
+		//TODO There is 100% a smarter way to remove a member from the lists
+		String leaver = leavingMember.getPublicKey();
+		for(Member a: herdMembers) {
+			if(a.getPublicKey().equals(leaver)) {
+				herdMembers.remove(leavingMember);
+			}
+		}
+		for(Member a: herdDrivers) {
+			if(a.getPublicKey().equals(leaver)) {
+				herdDrivers.remove(leavingMember);
+			}
+		}
+		for(Member a: herdSensors) {
+			if(a.getPublicKey().equals(leaver)) {
+				herdSensors.remove(leavingMember);
+			}
+		}
+		for(Member a: herdProcessors) {
+			if(a.getPublicKey().equals(leaver)) {
+				herdProcessors.remove(leavingMember);
+			}
+		}
+		for(Member a: herdViewers) {
+			if(a.getPublicKey().equals(leaver)) {
+				herdViewers.remove(leavingMember);
+			}
+		}
+		if(herdDestSetters != null) {
+			if (herdDestSetters.getPublicKey().equals(leaver)) {
+				herdDestSetters = null;
+			}
+		}
+		if(theLeader.getPublicKey().equals(leavingMember.getPublicKey())){
+		   nominateLeader();
+		}
+		return herdMembers;
+	}
+	
+//	public ArrayList<String> publishMembers() {
+//		for(String a: herdMembers){
+//		//TODO for each member in the herdMembers list publish the new list to them
+//		}
+//	}
+	
+	public String getHerdID () {
+		return herdID;
+	}
+	
+	/*
+	 * Retrieve the list of all Members of the Herd
+	 */
+	public ArrayList<Member> getMembers(){
+		return herdMembers;
+	}
+	
+	/*
+	 * Retrieve a single member from the List
+	 */
+	public Member getMember(String theKey) {
+		for(Member a: herdMembers) {
+			if(a.getPublicKey().equals(theKey)) {
+				return a;
+			}
+		}
+		return null;
+	}
+	
+	public ArrayList<Member> getDrivers() {
+		return herdDrivers;
+	}
+	
+	public ArrayList<Member> getProcessors(){
+		return herdProcessors;
+	}
+	
+	public ArrayList<Member> getSensors(){
+		return herdSensors;
+	}
+	
+	public ArrayList<Member> getViewers(){
+		return herdViewers;
+	}
+	
+	public Member getDestSetter(){
+		return herdDestSetters;
+	}
+}
---
+++ mcomp-Java/src/unitTesting/HerdTesting.java
@@ -0,0 +1,87 @@
+package unitTesting;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import common.Herd;
+import common.Member;
+
+/**
+ * 
+ * @author Stephen Pope 15836791
+ * 
+ * jUnit testing class for the Herd Object.
+ *
+ */
+public class HerdTesting {
+	@Before
+	public void setUp() {
+		
+	}
+	
+	/*
+	 * Tests the creation of a basic herd with one member added to it.
+	 */
+	@Test
+	public void create() {
+		String [] arr = new String [2];
+		arr [0] = "D";
+		arr [1] = "P";
+		Member a =  new Member(arr);
+		Herd b = new Herd (a);
+		assert(b.getMembers().size() == 1);
+		System.out.println(b.getHerdID());
+		System.out.println(b.getMembers());
+	}
+	
+	@Test
+	/*
+	 * Need to change the type of Array that passes values to 
+	 */
+	public void addManyMembers() {
+		String [] arr = {"D", "P"};
+		Member theMember = new Member(arr); 
+		Herd theHerd = new Herd(theMember);
+		ArrayList <Member> members = new ArrayList<Member>();
+		for (int i = 0; i < 5; i++) {
+			String [] abil = {"D", "P", "S"};
+			//TODO Fix the below function to produce various abilities for a Member
+/*			switch (i) {
+			case 0:
+				abil [0] = "D";
+				break;
+			case 1:
+				abil[0] = "D";
+				abil[1] = "P";
+				abil[2] = "S";
+				break;
+			case 2:
+				abil[0] = "V";
+				break;
+			case 3:
+				abil[0] = "V";
+				abil[1] = "W";
+				break;
+			case 4:
+				abil[0] = "D";
+				abil[1] = "P";
+				abil[2] = "S";
+				abil[3] = "V";
+				abil[4] = "W";
+				break;
+			}*/
+			for (int j = 0; j < 10; j++) {
+				members.add(new Member(abil));
+			}
+		}
+		for (Member a: members) {
+			a.joinHerd(theHerd);
+		}
+		System.out.println(theHerd.getMembers().size());
+		assert(theHerd.getMembers().size() == 51);
+	}
+	
+	
+
+}
