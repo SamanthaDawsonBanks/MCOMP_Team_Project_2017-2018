efe134d43b6f73e2dc42c4b58816a616d7f9fa73
DavidAveryUoB <d.avery1@uni.brighton.ac.uk>
2017-12-15 12:25:09 +0000

    Pathc (#27)
    * Commiting for Harry Jackson as he is without computer
    
    * cleaned unused imports, refactored PQueueItem to PathItem, imported
    PathItem
    
    * cleaned unused imports, refactored PQueueItem to PathItem, imported (#26)
    
    PathItem
    
    * Fix of PQueueItem > PathItem (missed it last time somehow)

--- mcomp_C/Path.cpp
+++ mcomp_C/Path.cpp
@@ -1,19 +1,77 @@
-/*
- * Path.cpp
- *
- *  Created on: 26 Nov 2017
- *      Author: David Avery
- */
-
 #include "Path.h"
+#ifndef NULL
+#define NULL   ((void *) 0)
+#endif
 
-Path::Path() {
-  // TODO Auto-generated constructor stub
-  //FIXME implement this once data structure is defined in java land
+Path::Path(Waypoint w) {
+  PathItem head;
+  PathItem destination;
+  PathItem lastItem;
+  int *ptr = NULL;
+  this->destination = new PathItem(w);
+  this->head = NULL;
+  this->lastItem = NULL;
+  this->length = 0;
 
 }
 
 Path::~Path() {
   // TODO Auto-generated destructor stub
 }
+
+
+void Path::addNode(Waypoint w){
+
+	if(length > 0){
+		PathItem = new PathItem(w);
+		lastItem.setNext(PathItem(w));
+		lastItem = lastItem.getNext();
+	}
+	else {
+		head = new PathItem(w);
+		lastItem = head;
+	}
+
+	length++;
+}
+
+
+Waypoint Path::poll(){
+
+	Waypoint res = destination.getData();
+	if(length > 0){
+		res = head.getData();
+		head = head.getNext();
+
+	if(head == NULL){
+		lastItem = NULL;
+	}
+	length--;
+	}
+	return res;
+}
+
+
+
+int Path::getLength() {
+	int counter = 0;
+	PathItem head1 = head;
+	if(head == NULL){
+		return 0;
+	}
+	while (head1 != NULL){
+		counter++;
+		head1 = head1.getNext();
+	}
+	return counter;
+}
+
+
+bool Path::isDone() {
+     return true;
+	 return(length == 0);
+
+}
+
+
 
