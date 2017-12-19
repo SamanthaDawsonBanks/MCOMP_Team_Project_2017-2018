88688948d0b1c164a9bb11b1f9ca69be34d93b28
Stephen Pope <stevempope@gmail.com>
2017-12-19 14:21:29 +0000

    Added Member TODO comments.

--- mcomp-Java/src/common/Member.java
+++ mcomp-Java/src/common/Member.java
@@ -34,6 +34,9 @@ public class Member {
 	 */
 
 	public Member (String[] can) {
+		/*TODO Change from list of Strings to enum to ensure Member 
+		 *cannot be set without any valid abilities.
+		 */
 		abilities = new ArrayList<String>();
 		herdMembers = new ArrayList<Member>();
 		if (can.length > 0) {
@@ -46,6 +49,7 @@ public class Member {
 				}
 			}
 		}
+		//TODO Generate a Public/Private Key Pair using Java Crypto Library.
 		publicKey = "Test";
 		privateKey = "Password123";
 
@@ -76,6 +80,12 @@ public class Member {
 	 * @param The Herd object being joined.
 	 */
 	public void joinHerd(Herd h) {
+		/*TODO Find a sufficient replacement for handing the
+		 * Herd object to this method. We need a method that
+		 * allows the Herd to "advertise" itself. The Member
+		 * should be able to respond to this advertisement 
+		 * when appropriate. 
+		 */
 		herdMembers = h.requestJoin(this);
 		if (herdMembers.isEmpty() == true) {
 			System.out.println("Failed to join Herd!");
@@ -90,7 +100,9 @@ public class Member {
 	 * @param The Herd being left.
 	 */
 	public void leaveHerd(Herd h) {
-		//TODO Will need access to notify the Leader
+		/*TODO Will need to replace the Herd object
+		 * param with an IP or similar.
+		 */
 		h.requestLeave(this);
 		System.out.printf("%s has left the Herd %s \n",this, h.getHerdID());
 	}
@@ -102,6 +114,8 @@ public class Member {
 	 */
 	public void becomeLeader() {
 		//TODO Needs to have access to Herd ID/IP?
+		//TODO Write this method.
+		//TODO Need to create a Leader Object?
 	}
 	
 	/**
@@ -120,6 +134,11 @@ public class Member {
 	 * @param The new HerdID.
 	 */
 	public void setHerdID(String newID) {
+		/*TODO Write a check to ensure that the 
+		 * change of ID is valid. Perhaps contact
+		 * the Herd ID supplied. Perhaps ensure that
+		 * the Herd is the only one able to change this.
+		 */
 		herdID = newID;
 	}
 }
