653d9802f125190aae6f6beb2a8433e17df7ef9f
RyanShoobertUoB <shoobertr@gmail.com>
2018-01-30 20:22:14 +0000

    - Updated javadoc comments for initial RMI

--- mcomp_Scratchpad/src/server/ServerMain.java
+++ mcomp_Scratchpad/src/server/ServerMain.java
@@ -4,6 +4,10 @@ package server;
  * 
  * @author Ryan Shoobert (15812407)
  * 
+ * Main entry point for starting an instance of the server. To create this, a port number
+ * and a name for the instance must be parsed in before the start method can be called
+ * to set up a registry and bind behaviour for clients to access.
+ * 
  */
 public class ServerMain {
   public static void main(String[] args) throws Exception {
