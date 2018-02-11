ecb43e1400e1b9ba9f7094483925cb6a37bc9642
Stephen Pope <stevempope@gmail.com>
2018-02-11 20:55:27 +0000

    Added Leader Class and IP Address publishing.

--- mcomp_Java/src/common/interfaces/Leadable.java
+++ mcomp_Java/src/common/interfaces/Leadable.java
@@ -3,9 +3,11 @@
  */
 package common.interfaces;
 
+import java.net.InetAddress;
+
 /**
  * @author David Avery
- *
+ * @author Stephen Pope 15836791
  */
 public interface Leadable {
 
@@ -13,8 +15,9 @@ public interface Leadable {
    * 
    */
   //pass in first bot?? member?? self??
-  // TODO Auto-generated constructor stub
   //Data type?? array?? array list?? queue?? (q means leader = first in / oldest member??)
-  Membership nominateLeader();  
+  Membership nominateLeader();
+  InetAddress [] getAddress();
+  InetAddress publishAddress();
   
 }
