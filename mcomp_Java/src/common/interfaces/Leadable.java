f347d0189d5fa8e82e42482287022eb74f2b73eb
Stephen Pope <stevempope@gmail.com>
2018-02-11 20:59:28 +0000

    Added address methods to interface

--- mcomp_Java/src/common/interfaces/Leadable.java
+++ mcomp_Java/src/common/interfaces/Leadable.java
@@ -15,6 +15,7 @@ public interface Leadable {
    * 
    */
   //pass in first bot?? member?? self??
+  //IP Address publishing added
   //Data type?? array?? array list?? queue?? (q means leader = first in / oldest member??)
   Membership nominateLeader();
   InetAddress [] getAddress();
