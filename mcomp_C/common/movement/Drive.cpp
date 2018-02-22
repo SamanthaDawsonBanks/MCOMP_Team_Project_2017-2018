1f134a04b27d4f30a5e6372ef07a62abc326e6ad
Stephen Pope <stevempope@gmail.com>
2018-02-22 14:28:34 +0000

    Added Drive h and CPP file

---
+++ mcomp_C/common/movement/Drive.cpp
@@ -0,0 +1,9 @@
+/*
+ * Waypoint.cpp
+ *
+ *  Created on: 26 Nov 2017
+ *      Author: David Avery
+ */
+
+#include "Drive.h"
+
---
+++ mcomp_C/common/movement/Drive.h
@@ -0,0 +1,19 @@
+/*
+ * Waypoint.h
+ *
+ *  Created on: 22 February 2018
+ *      Authors: Stephen Pope 15836791
+ *      		 Harry Jackson 14812630
+ */
+
+#ifndef DRIVE_H_
+#define DRIVE_H_
+
+class Drive{
+ private:
+
+ public:
+
+};
+
+#endif /* DRIVE_H_ */
