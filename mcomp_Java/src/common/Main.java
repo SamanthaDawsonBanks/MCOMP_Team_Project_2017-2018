0e50c68138ebd1998819ce9682f343c45c61ee09
Stephen Pope <stevempope@gmail.com>
2018-02-23 11:25:10 +0000

    Main creates member, polls abilities

--- mcomp_Java/src/common/Main.java
+++ mcomp_Java/src/common/Main.java
@@ -1,16 +1,17 @@
 package common;
 
 import java.util.ArrayList;
-import java.util.Queue;
-
 import common.datatypes.Ability;
+import common.objects.Member;
 
 public class Main {
 
 	public static void main (Ability [] args) {
-		ArrayList<Ability> skills = new ArrayList();
-		for(Ability a : args) {
-			skills.add(a);			
+		Member me = new Member(args);
+		ArrayList<Ability> skills = me.getAbilities();
+		for(Ability a : skills) {
+				
+			}
 		}
 	}
 }
