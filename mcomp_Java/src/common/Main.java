517f922c508bdb121e6d075b4b175f4c17372acd
Stephen Pope <stevempope@gmail.com>
2018-02-23 11:29:11 +0000

    Added View class and call for viewer

--- mcomp_Java/src/common/Main.java
+++ mcomp_Java/src/common/Main.java
@@ -3,6 +3,7 @@ package common;
 import java.util.ArrayList;
 import common.datatypes.Ability;
 import common.objects.Member;
+import ui.View;
 
 public class Main {
 
@@ -10,7 +11,9 @@ public class Main {
 		Member me = new Member(args);
 		ArrayList<Ability> skills = me.getAbilities();
 		for(Ability a : skills) {
-				
+				if (a.equals(common.datatypes.Ability.VIEWER)) {
+					View theObserver = new View(me);
+				}
 			}
 		}
 	}
