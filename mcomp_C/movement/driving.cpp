2c684a10e779b8ef2351851bdde2248b5905695d
Stephen Pope <stevempope@gmail.com>
2018-03-11 21:21:57 +0000

    Explainer for release

--- mcomp_C/movement/driving.cpp
+++ mcomp_C/movement/driving.cpp
@@ -30,11 +30,15 @@ driving::~driving() {
 }
 
 void releaseMotors(){
+	//The purpose of this will be for safety when STOPPING the bot.
+	//When the robot finishes pulsing the motors, they remain locked.
+	//This will be used to allow us to transport the robot.
 	leftMotor.release();
 	rightMotor.release();
 }
 
 Waypoint driveTo(Waypoint w){
+
 	return nullptr;
 }
 
