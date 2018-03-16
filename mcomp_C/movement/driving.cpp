a7e57143bbb2d0c0713e485be5dceadff073b738
Stephen Pope <stevempope@gmail.com>
2018-03-16 13:46:47 +0000

    Updated Comments

--- mcomp_C/movement/driving.cpp
+++ mcomp_C/movement/driving.cpp
@@ -16,8 +16,8 @@ driving::driving() {
 	AFMS = Adafruit_MotorShield(); //The MotorShield
 	leftMotor = AFMS.getStepper(200,1); //360(div)1.8 degree steps, M1 and M2 on the MotorShield
 	rightMotor = AFMS.getStepper(200, 2); //M3 and M4 on the MotorShield
-	leftMotor.setSpeed(30); //Set speed of rotation in RPM, 100Hz * 1.8 Degrees = 1 rotation of wheel every 2 seconds.
-	//TODO change to 400?
+	leftMotor.setSpeed(30); //Set speed of rotation in RPM, 100Hz * 1.8 Degrees = 1 rotation of wheel every 2 seconds, or 30 RPM.
+	//TODO change to 400Hz, or 120 rpm?
 	rightMotor.setSpeed(30);
 }
 
