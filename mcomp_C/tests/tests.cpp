e44be006b50539eef80e4ed241127e3b2f711486
DavidAveryUoB <d.avery1@uni.brighton.ac.uk>
2018-03-20 10:35:14 +0000

    pulled cpp tests to seperate file (#122)

---
+++ mcomp_C/tests/tests.cpp
@@ -0,0 +1,112 @@
+/*
+ * tests.cpp
+ *
+ *  Created on: 20 Mar 2018
+ *      Author: David Avery 15823926
+ */
+
+#include "tests.h"
+
+
+//--------------------------------------Tests--------------------------------------
+void testPath() {
+  Serial.begin(9600);
+  delay(1000);
+  Serial.println("Begin testPath");
+
+  Waypoint q;
+
+  Waypoint d(100, 100);
+  Path p(d);
+
+  q = p.poll();
+  Serial.print(q.getX());
+  Serial.print(",");
+  Serial.print(q.getY());
+  Serial.println("");
+
+  p.addNode(Waypoint(200, 200));
+
+  q = p.poll();
+  Serial.print(q.getX());
+  Serial.print(",");
+  Serial.print(q.getY());
+  Serial.println("");
+
+  q = p.poll();
+  Serial.print(q.getX());
+  Serial.print(",");
+  Serial.print(q.getY());
+  Serial.println("");
+
+  p.addNode(Waypoint(250, 250));
+  p.addNode(Waypoint(350, 350));
+
+  q = p.poll();
+  Serial.print(q.getX());
+  Serial.print(",");
+  Serial.print(q.getY());
+  Serial.println("");
+
+  q = p.poll();
+  Serial.print(q.getX());
+  Serial.print(",");
+  Serial.print(q.getY());
+  Serial.println("");
+
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++) {
+      Waypoint x(i, j);
+      p.addNode(x);
+    }
+  }
+
+  for (int k = 0; k < 10; k++) {
+    for (int l = 0; l < 10; l++) {
+      q = p.poll();
+      Serial.print(q.getX());
+      Serial.print(",");
+      Serial.print(q.getY());
+      Serial.println("");
+    }
+  }
+
+  Serial.println("end of data");
+  q = p.poll();
+  Serial.print(q.getX());
+  Serial.print(",");
+  Serial.print(q.getY());
+  Serial.println("");
+
+  q = p.poll();
+  Serial.print(q.getX());
+  Serial.print(",");
+  Serial.print(q.getY());
+  Serial.println("");
+
+  q = p.poll();
+  Serial.print(q.getX());
+  Serial.print(",");
+  Serial.print(q.getY());
+  Serial.println("");
+
+  Serial.println("End testPath");
+}
+
+void testWaypoint() {
+  Serial.begin(9600);
+  delay(1000);
+  Serial.println("Begin testWaypoint");
+
+  Waypoint t(0, 0);  // = new Waypoint(0,0);
+  Serial.println("end of data input");
+  Serial.print(t.getX());
+  Serial.print(",");
+  Serial.print(t.getY());
+  Serial.println("");
+
+  Serial.println("End testWaypoint");
+}
+
+
+
---
+++ mcomp_C/tests/tests.h
@@ -0,0 +1,18 @@
+/*
+ * tests.h
+ *
+ *  Created on: 20 Mar 2018
+ *      Author: David Avery 15823926
+ */
+
+
+#include "Arduino.h"
+#include "../common/datatypes/path/Path.h"
+
+#ifndef TESTS_H_
+#define TESTS_H_
+
+void testPath();
+void testWaypoint();
+
+#endif /* TESTS_H_ */
