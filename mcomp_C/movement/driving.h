d00c759d0c6ca2fb192561eb3b20d45810de0487
Stephen Pope <stevempope@gmail.com>
2018-03-20 11:24:04 +0000

    Controlling the Motors (#121)
    * Initial config for Driving Class
    
    * Added AdafruitLibrary
    
    * Testing
    
    * probably fixed
    
    * Added MotorShield initialisation in constructor.
    
    * Made variables outside of constructor for use outside of constructor
    
    * Added Destructor
    
    * Added driveTo stub
    
    * Explainer for release
    
    * Added stubs to pulse the motor
    
    * Added drive logic
    
    * Added space for maths
    
    * Added logic for turning
    
    * Added maths for returning current position.
    
    * Added turning, corrected forward movement bug
    
    * Updated Comments
    
    * fixed forward drive maths?
    
    * Explained Maths
    
    * Added compass todo
    
    * Added more comments ready for merge
    
    * Update .cproject
    
    * Update .cproject
    
    * Update .cproject
    
    * Update .cproject
    
    * Update .project
    
    * Update org.eclipse.cdt.core.prefs
    
    * Update org.eclipse.cdt.core.prefs
    
    * Update org.eclipse.cdt.core.prefs
    
    * Update org.eclipse.cdt.core.prefs
    
    * Update org.eclipse.cdt.core.prefs
    
    * Update org.eclipse.cdt.core.prefs
    
    * Update org.eclipse.cdt.core.prefs
    
    * Update org.eclipse.cdt.core.prefs
    
    * Update org.eclipse.cdt.core.prefs

---
+++ mcomp_C/movement/driving.h
@@ -0,0 +1,25 @@
+/*
+ * driving.h
+ *
+ *  Created on: 11 Mar 2018
+ *      Author: Steve
+ */
+
+#ifndef MOVEMENT_DRIVING_H_
+#define MOVEMENT_DRIVING_H_
+#include "../common/datatypes/Waypoint.h"
+#include <Wire.h>
+#include "../libraries/Adafruit_MS_PWMServoDriver.h"
+#include "../libraries/Adafruit_MotorShield.h"
+
+class driving {
+public:
+	driving();
+	~driving();
+	Waypoint driveTo(Waypoint);
+	void unlockMotors();
+
+};
+
+
+#endif /* MOVEMENT_DRIVING_H_ */
