26b633026a1e998f12c760a13f58e3b3731832b3
Stephen Pope <stevempope@gmail.com>
2018-03-21 10:38:37 +0000

    Added description and methods for traversable.

--- mcomp_Scratchpad/src/interfaces_march_2018/Traversable.java
+++ mcomp_Scratchpad/src/interfaces_march_2018/Traversable.java
@@ -1,5 +1,21 @@
 package interfaces_march_2018;
 
-public interface Traversable {
+/**
+ * @author Stephen Pope 15836791
+ * @author David Avery
+ * @author Ryan Shoobert 15812407
+ * @author Harry Jackson
+ * 
+ * Traversable interface
+ * 
+ * An object that is traversable is able be queried for sections of itself
+ * so that they can be analysed by a pathfinding algorithm.
+ * 
+ */
 
+public interface Traversable {
+	public Vertex getVertex(Waypoint w);
+	public void setBlocked();
+	public int getX();
+	public int getY();
 }
