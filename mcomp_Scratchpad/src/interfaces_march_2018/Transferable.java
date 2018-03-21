d7f49b676394bb482534e1bc83d605a5226db4ad
Stephen Pope <stevempope@gmail.com>
2018-03-21 10:07:08 +0000

    Added description for transferable interface

--- mcomp_Scratchpad/src/interfaces_march_2018/Transferable.java
+++ mcomp_Scratchpad/src/interfaces_march_2018/Transferable.java
@@ -1,5 +1,19 @@
 package interfaces_march_2018;
 
+/**
+ * @author Stephen Pope 15836791
+ * @author David Avery
+ * @author Ryan Shoobert 15812407
+ * @author Harry Jackson
+ * 
+ * Transferable interface
+ * 
+ * An object is transferable if it able to be handed from one Herd to another
+ * by the Leader of its original Herd.
+ */
+
 public interface Transferable {
+	
+	
 
 }
