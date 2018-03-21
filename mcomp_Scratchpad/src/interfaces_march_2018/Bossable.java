32a26035adf308ba01564497b9b63ca8d7d7a37e
Stephen Pope <stevempope@gmail.com>
2018-03-21 09:57:00 +0000

    Added description and methods for Bossable Interface

--- mcomp_Scratchpad/src/interfaces_march_2018/Bossable.java
+++ mcomp_Scratchpad/src/interfaces_march_2018/Bossable.java
@@ -1,5 +1,23 @@
 package interfaces_march_2018;
+import common.datatypes.map.*; //TODO these wont work until we move into the MCOMP Java folder
+import common.datatypes.path.*;
 
+/**
+ * @author Stephen Pope 15836791
+ * @author David Avery
+ * @author Ryan Shoobert 15812407
+ * @author Harry Jackson
+ * 
+ * Bossable interface
+ * 
+ * A bossable object is one that can receive instructions from the leader of a herd
+ * to process a section of a particular item, either a segment of a map to be augmented
+ * or a path to be analysed.
+ * 
+ */
 public interface Bossable {
+
+	public Map processMapLump(Map);
+	public Path processPathLump(Path);	
 
 }
