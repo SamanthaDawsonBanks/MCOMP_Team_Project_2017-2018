09c88dd1e7f6627ef097e7ae4e1bdf80e125c6ab
Stephen Pope <stevempope@gmail.com>
2018-03-21 10:15:02 +0000

    Added description and method for Directable

--- mcomp_Scratchpad/src/interfaces_march_2018/Directable.java
+++ mcomp_Scratchpad/src/interfaces_march_2018/Directable.java
@@ -1,5 +1,21 @@
 package interfaces_march_2018;
 
+import common.datatpyes.Waypoint;
+
+/**
+ * @author Stephen Pope 15836791
+ * @author David Avery
+ * @author Ryan Shoobert 15812407
+ * @author Harry Jackson
+ * 
+ * Directable interface
+ * 
+ * A directable object is one that can accept input that defines a final
+ * goal or destination for the Herd to attempt to pathfind to.
+ */
+
 public interface Directable {
 
+	public boolean setDestination(Waypoint w); 
+	
 }
