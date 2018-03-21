f01fa42a4c50404bd932283436af8d9ca0d062c9
Stephen Pope <stevempope@gmail.com>
2018-03-21 11:04:57 +0000

    Added description and methods for Connectable interface.

--- mcomp_Scratchpad/src/interfaces_march_2018/Connectable.java
+++ mcomp_Scratchpad/src/interfaces_march_2018/Connectable.java
@@ -1,5 +1,22 @@
 package interfaces_march_2018;
 
+/**
+ * @author Stephen Pope 15836791
+ * @author David Avery
+ * @author Ryan Shoobert 15812407
+ * @author Harry Jackson
+ * 
+ * Connectable interface
+ * 
+ * An object is connectable if it assumes the role of an RMI server,
+ * meaning it can accept connections from multiple clients who wish
+ * to register their methods with the RMI registry.
+ * 
+ */
+
 public interface Connectable {
 
+	public void register();
+	public void deregister();
+	
 }
