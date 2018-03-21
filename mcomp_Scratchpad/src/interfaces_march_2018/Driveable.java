85c394989cb994f27655d5e31bf95cb24a354491
Stephen Pope <stevempope@gmail.com>
2018-03-21 10:26:04 +0000

    Added description and method for driveable

--- mcomp_Scratchpad/src/interfaces_march_2018/Driveable.java
+++ mcomp_Scratchpad/src/interfaces_march_2018/Driveable.java
@@ -1,5 +1,22 @@
 package interfaces_march_2018;
 
+import common.datatypes.Waypoint;
+
+/**
+ * @author Stephen Pope 15836791
+ * @author David Avery
+ * @author Ryan Shoobert 15812407
+ * @author Harry Jackson
+ * @author Steve
+ * 
+ * Driveable interface
+ * 
+ * A driveable object can take a given Waypoint relative to its position and
+ * drive to that location.
+ */
+
 public interface Driveable {
 
+	public Waypoint drive (Waypoint w);
+	
 }
