d5e4d71845ff847a7c4cbd3e76e2a30d0472dc02
Stephen Pope <stevempope@gmail.com>
2018-03-21 10:29:19 +0000

    Added description and methods for Senseable

--- mcomp_Scratchpad/src/interfaces_march_2018/Senseable.java
+++ mcomp_Scratchpad/src/interfaces_march_2018/Senseable.java
@@ -1,5 +1,20 @@
 package interfaces_march_2018;
 
-public interface Senseable {
+/**
+ * @author Stephen Pope 15836791
+ * @author David Avery
+ * @author Ryan Shoobert 15812407
+ * @author Harry Jackson
+ *
+ * Senseable interface
+ * 
+ * An object is Senseable if it is equipped to add detail to a Map
+ * with data from its sensors.
+ * 
+ */
 
+public interface Senseable {
+	
+	public void sense();
+	
 }
