f6aa3bae03f1135197b0113b7e37030dfa961991
Stephen Pope <stevempope@gmail.com>
2018-03-21 10:11:57 +0000

    Added joinable description and methods. RAISE ISSUE

--- mcomp_Scratchpad/src/interfaces_march_2018/Joinable.java
+++ mcomp_Scratchpad/src/interfaces_march_2018/Joinable.java
@@ -1,5 +1,19 @@
 package interfaces_march_2018;
 
+/**
+ * @author Stephen Pope 15836791
+ * @author David Avery
+ * @author Ryan Shoobert 15812407
+ * @author Harry Jackson
+ * 
+ * Joinable interface
+ * 
+ * An object is joinable if it has the ability to accept new Members to its Herd.
+ *
+ */
+
 public interface Joinable {
 
+	public boolean joinHerd();//TODO leader info? Is this interface badly specified?
+	public boolean notifyJoin();//TODO same as above, notify the leader of a join?
 }
