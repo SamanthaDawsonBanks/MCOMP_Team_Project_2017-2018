aa3f321ad405fdb39f0103495cc943fed42e9703
Stephen Pope <stevempope@gmail.com>
2018-03-21 10:54:00 +0000

    Added description and method for updateable.

--- mcomp_Scratchpad/src/interfaces_march_2018/Updateable.java
+++ mcomp_Scratchpad/src/interfaces_march_2018/Updateable.java
@@ -1,5 +1,19 @@
 package interfaces_march_2018;
 
+/**
+ * @author Stephen Pope 15836791
+ * @author David Avery
+ * @author Ryan Shoobert 15812407
+ * @author Harry Jackson
+ *
+ * Updateable interface
+ * 
+ * An object is updateable if it represents the model in the MVC pattern.
+ * 
+ */
+
 public interface Updateable {
+	
+	public void updateClient();
 
 }
