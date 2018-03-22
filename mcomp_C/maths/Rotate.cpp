6fb870d5ecd9fffd46da4837b0cbb2af73c54a9c
Stephen Pope <stevempope@gmail.com>
2018-03-22 16:26:20 +0000

    Added turn motor code.

--- mcomp_C/maths/Rotate.cpp
+++ mcomp_C/maths/Rotate.cpp
@@ -11,11 +11,19 @@ double rotate (double theta){
 
 	double bodyRotation = ((2 * M_PI) * (wheelTrack/2)) * (theta/360);
 	double pulseDistance = ((2 * M_PI) * wheelSize) * (stepsInRev/360);
-}
+	int turnPulses = (int) bodyRotation / pulseDistance;
 
-}
+	for(int i = 0; i > 0; i--){
+		if (theta> 0){ //Circle theory left is negative?
+			leftMotor.onestep(FORWARD, DOUBLE); //Turns the bot right
+			rightMotor.onestep(BACKWARD, DOUBLE);
+		}
 
-Rotate::~Rotate() {
-	// TODO Auto-generated destructor stub
-}
+		else{
+			leftMotor.onestep(BACKWARD, DOUBLE); //Turns the bot left
+			rightMotor.onestep(FORWARD, DOUBLE);
+		}
+
+	}
+
 
