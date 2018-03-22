276414d0ab092e40ca2eb672eb20ab402fb2cd40
Stephen Pope <stevempope@gmail.com>
2018-03-22 17:11:05 +0000

    Added drive logic

--- mcomp_C/maths/Forward.cpp
+++ mcomp_C/maths/Forward.cpp
@@ -8,6 +8,17 @@
 #include "Forward.h"
 
 long forward (long distance){
-
+	bool ultraSoundTest = false;
+	double pulseTravel = wheelSize / stepsInRev;
+	int numForwardSteps = (int) distance / pulseTravel;
+//TODO Factor i out of this! See Uber
+	for(int i = numForwardSteps; i > 0; i--){
+			if(ultraSoundTest == true){ //TODO Interrupt needs adding
+				return numForwardSteps - i * pulseTravel;
+			}
+			leftMotor.onestep(FORWARD, DOUBLE); //motor.step(number of steps, FORWARD or BACKWARD, SINGLE DOUBLE or INTERLEAVED)
+			rightMotor.onestep(FORWARD, DOUBLE);//motor.onestep(Direction, Type);
+		}
+	return distance;
 }
 
