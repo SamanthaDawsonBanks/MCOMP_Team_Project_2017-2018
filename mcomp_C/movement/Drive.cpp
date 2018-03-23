ac7ecbdc66e395d4200677d10b895b5e547978e4
Stephen Pope <stevempope@gmail.com>
2018-03-23 09:39:50 +0000

    Added Drive comments

--- mcomp_C/movement/Drive.cpp
+++ mcomp_C/movement/Drive.cpp
@@ -3,6 +3,23 @@
  *
  *  Created on: 22 Mar 2018
  *      Author: Stephen Pope 15836791
+ *
+ *
+ * Drive method
+ *
+ * This method allows a robot to drive from its current position (assumed to be 0,0) to
+ * the location handed to it. The position relative to the world is not handled by this
+ * method, that is handled in the partner Java application.
+ *
+ * In order to drive to the next point along a Path, the robot needs to assess the angle
+ * it needs to turn by to point at the Waypoint provided, then how far it needs to travel
+ * to reach it. As a Path is split into small sections by the Java code, we can assume
+ * that only one rotation and forward movement are required.
+ *
+ * If the journey is successful and the robot reaches its destination, it returns the
+ * Waypoint it was handed. If the robot becomes blocked along its path, then the robot
+ * returns a Waypoint with a value representing how far it was able to travel, effectively
+ * its new location relative to where it started the current drive call.
  */
 
 #include "Drive.h"
