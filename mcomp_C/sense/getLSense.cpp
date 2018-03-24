48c7d40576d069c280e13da53cc84a60d0abeec0
David Avery <D.Avery1@uni.brighton.ac.uk>
2018-03-24 13:19:41 +0000

    setup file system

---
+++ mcomp_C/sense/getLSense.cpp
@@ -0,0 +1,12 @@
+/*
+ * LSense.cpp
+ *
+ *  Created on: 24 Mar 2018
+ *      Author: David Avery 15823926
+ */
+
+#include "getLSense.h"
+
+void getLSense() {
+
+}
---
+++ mcomp_C/sense/getLSense.h
@@ -0,0 +1,13 @@
+/*
+ * LSense.h
+ *
+ *  Created on: 24 Mar 2018
+ *      Author: David Avery 15823926
+ */
+
+#ifndef SENSE_GETLSENSE_H_
+#define SENSE_GETLSENSE_H_
+
+void getLSense();
+
+#endif /* SENSE_GETLSENSE_H_ */
