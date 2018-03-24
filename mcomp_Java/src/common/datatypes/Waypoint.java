abf74bc59b893f30880446546742f67f0fef794e
David Avery <D.Avery1@uni.brighton.ac.uk>
2018-03-24 15:14:48 +0000

    Add AD>WP on J side

--- mcomp_Java/src/common/datatypes/Waypoint.java
+++ mcomp_Java/src/common/datatypes/Waypoint.java
@@ -3,6 +3,8 @@
  */
 package common.datatypes;
 
+import common.datatypes.AngleDistance;
+
 /**
  * @author David Avery
  * 
@@ -16,24 +18,35 @@ public class Waypoint {
   private boolean toBeBlocked;
 
   /**
- * 
- * @param newX
- * @param newY
- */
-    public Waypoint(double x, double y) {
-      this(x, y, true);
+   * 
+   * @param newX
+   * @param newY
+   */
+  public Waypoint(double x, double y) {
+    this(x, y, true);
   }
 
-    /**
-     * 
-     * @param newX
-     * @param newY
-     */
-        public Waypoint(double x, double y, boolean b) {
-        this.x = x;
-        this.y = y;
-        this.toBeBlocked = b;
-      }
+  /**
+   * 
+   * @param newX
+   * @param newY
+   */
+  public Waypoint(double x, double y, boolean b) {
+    this.x = x;
+    this.y = y;
+    this.toBeBlocked = b;
+  }
+
+  /**
+   * 
+   * @param AngleDistance a
+   */
+  public Waypoint(AngleDistance a) {
+    // TODO Auto-generated constructor stub
+    a.getDistance();
+    y = a.getDistance() * sin(a.getTheta());
+    x = a.getDistance() * cos(a.getTheta());
+  }
 
   /**
    * Data Type definition
