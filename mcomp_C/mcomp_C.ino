07c472533585c607d62080a775632261641afaa3
Stephen Pope <stevempope@gmail.com>
2018-03-26 16:05:14 +0100

    Refactored propulsion and added current heading

--- mcomp_C/mcomp_C.ino
+++ mcomp_C/mcomp_C.ino
@@ -10,7 +10,6 @@
 // any required libraries
 #include "Arduino.h"
 #include "tests/tests.h"
-#include "libraries/Adafruit_MotorShield.h"
 
 //-----------------------------------Definitions-----------------------------------
 // eg. #define RED 13
@@ -24,9 +23,6 @@
 // Remember you ONLY have 2k system RAM
 // eg. boolean personWaiting = true;
 
-Adafruit_MotorShield* AFMS;
-Adafruit_StepperMotor* leftMotor;
-Adafruit_StepperMotor* rightMotor;
 
 //----------------------------Global Volatile Variables----------------------------
 // Volatile variables used by interrupts
@@ -43,13 +39,6 @@ void setup() {
   // TODO method stub
   testWaypoint();
   testPath();
-  //MotorShield Setup
-  AFMS = new Adafruit_MotorShield(); //The MotorShield
-  leftMotor = (*AFMS).getStepper(200,1); //360(div)1.8 degree steps, M1 and M2 on the MotorShield
-  rightMotor = (*AFMS).getStepper(200, 2); //M3 and M4 on the MotorShield
-  (*leftMotor).setSpeed(30); //Set speed of rotation in RPM, 100Hz * 1.8 Degrees = 1 rotation of wheel every 2 seconds, or 30 RPM.
-  //TODO change to 400Hz, or 120 rpm?
-  (*rightMotor).setSpeed(30);
 }  //End Setup
 
 //-----------------------------Functions / Subroutines-----------------------------
--- mcomp_C/movement/Propulsion.cpp
+++ mcomp_C/movement/Propulsion.cpp
@@ -2,17 +2,85 @@
  * Propulsion.cpp
  *
  *  Created on: 26 Mar 2018
- *      Author: Steve
+ *      Author: Stephen Pope 15836791
  */
 
 #include "Propulsion.h"
+#include "..\common\datatypes\AngleDistance.h"
+#include "..\config\robot_config.h"
 
 Propulsion::Propulsion() {
-  // TODO Auto-generated constructor stub
-
+  //MotorShield Setup
+  AFMS = Adafruit_MotorShield(); //The MotorShield
+  leftMotor = (*AFMS.getStepper(200,1)); //360(div)1.8 degree steps, M1 and M2 on the MotorShield
+  rightMotor = (*AFMS.getStepper(200, 2)); //M3 and M4 on the MotorShield
+  leftMotor.setSpeed(30); //Set speed of rotation in RPM, 100Hz * 1.8 Degrees = 1 rotation of wheel every 2 seconds, or 30 RPM.
+  //TODO change to 400Hz, or 120 rpm?
+  rightMotor.setSpeed(30);
+  currentHeading = 0; //TODO Constructor may need to take a starting angle.
 }
 
 Propulsion::~Propulsion() {
   // TODO Auto-generated destructor stub
+}
+
+Waypoint Propulsion::Drive (Waypoint w){
+  /**
+   * atan2 values, 0 = East (Positive X axis), M_PI/2 = North (Positive Y Axis)
+   *         Negative M_PI/2 = South (Negative Y Axis)
+   *         M_PI = West (Negative X Axis)
+   * See http://en.cppreference.com/w/cpp/numeric/math/atan2 for more.
+   * Values returned are in Radians, so value * 180/pi = degrees
+   */
+  AngleDistance movement = w.toAngleDistance();
+  currentHeading += rotate(movement.getTheta()-currentHeading);
+  long distance = forward(movement.getDistance());
+  return Waypoint((distance/movement.getDistance()) * w.getX(), (distance/movement.getDistance()) * w.getY());
+}
+
+
+double Propulsion::rotate (double theta){
+
+  double bodyRotation = ((2 * M_PI) * (wheelTrack/2)) * (theta/360);
+  double pulseDistance = ((2 * M_PI) * wheelSize) * (stepsInRev/360);
+  int turnPulses = (int) abs(bodyRotation / pulseDistance);
+
+  for(int i = turnPulses; i > 0; i--){
+    if (theta < 0){
+      leftMotor.onestep(FORWARD, DOUBLE); //Turns the bot right
+      rightMotor.onestep(BACKWARD, DOUBLE);
+    }
+    else{
+      leftMotor.onestep(BACKWARD, DOUBLE); //Turns the bot left
+      rightMotor.onestep(FORWARD, DOUBLE);
+    }
+  }
+  return theta;
+}
+
+
+long Propulsion::forward (long distance){
+  bool ultraSoundTest = false;
+  double pulseTravel = wheelSize / stepsInRev;
+  int numForwardSteps = (int) distance / pulseTravel;
+  //TODO Factor i out of this! See Uber
+  for(int i = numForwardSteps; i > 0; i--){
+    if(ultraSoundTest == true){ //TODO Interrupt needs adding
+      return numForwardSteps - i * pulseTravel;
+    }
+    leftMotor.onestep(FORWARD, DOUBLE); //motor.step(number of steps, FORWARD or BACKWARD, SINGLE DOUBLE or INTERLEAVED)
+    rightMotor.onestep(FORWARD, DOUBLE);//motor.onestep(Direction, Type);
+  }
+  return distance;
+}
+
+
+void Propulsion::releaseMotors(){
+  //The purpose of this will be for safety when STOPPING the bot.
+  //When the robot finishes pulsing the motors, they remain locked.
+  //This will be used to allow us to transport the robot.
+  leftMotor.release();
+  rightMotor.release();
+
 }
 
