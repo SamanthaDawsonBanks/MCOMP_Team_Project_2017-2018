0696f64ec407d39bcc1d3b53f56d336734001757
Stephen Pope <stevempope@gmail.com>
2018-03-27 10:15:08 +0100

    Made release motors a public method

--- mcomp_C/movement/Propulsion.h
+++ mcomp_C/movement/Propulsion.h
@@ -18,6 +18,7 @@ class Propulsion {
   virtual ~Propulsion();
   Waypoint Drive (Waypoint);
   double getHeading();
+  void releaseMotors();
 
  private:
   Adafruit_MotorShield AFMS;
@@ -26,7 +27,6 @@ class Propulsion {
   double currentHeading;
   double rotate(double);
   long forward(long);
-  void releaseMotors();
 };
 
 #endif /* MOVEMENT_PROPULSION_H_ */
