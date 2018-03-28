8365c9da1e53a68375cd372df473ca6f319a3749
RyanShoobertUoB <shoobertr@gmail.com>
2018-03-28 20:28:07 +0100

    Work for 28th March
    -Much of Pipe.java implemented but still not completely tested/working
    
    Still to complete:
    -Reader/Write at the Arduino end
    -Implementation of Base64 encoder/decoder at Arduino end
    -Unit testing

--- mcomp_Java/src/member/coms/Pipe.java
+++ mcomp_Java/src/member/coms/Pipe.java
@@ -1,9 +1,10 @@
-package member.coms;
-
-import java.util.Base64;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.ArrayList;
 // encoder/decoder imports
-import java.util.Base64.Decoder;
-import java.util.Base64.Encoder;
+import java.util.Base64;
+// serial communication library import
+import com.fazecast.jSerialComm.*;
 
 /**
  * 
@@ -12,9 +13,35 @@ import java.util.Base64.Encoder;
  */
 public class Pipe {
 
-  public Pipe() {
-    // TODO Constructor/setup related code
-    //Set up serial connection bits
+  private SerialPort p;
+  private String comPortName;
+  private final int BAUD_RATE = 9600;
+
+  private PrintWriter comWriter;
+  private Scanner comReader;
+
+  private ArrayList<Byte> buffer = new ArrayList<Byte>();
+
+  /**
+   * DOCME
+   * 
+   * @param comPortName
+   */
+  public Pipe(String comPortName) {
+    // Set up serial connection bits
+
+    this.comPortName = comPortName;
+
+    // will need to be more robust way of being certain about this but will assume
+    // on my system for now to be COM3
+    p = SerialPort.getCommPort(this.comPortName);
+
+    // assume default baud rate for arduino board
+    p.setBaudRate(BAUD_RATE);
+
+    // may factor into separate method(s) with additional checks or failure - will
+    // work on this
+    p.openPort();
   }
 
   /**
@@ -23,53 +50,79 @@ public class Pipe {
    * @return 0 if the serial is empty; > 0 if there is still data waiting to be processed
    */
   public int available() {
-    //where is the buffer?
-    throw new UnsupportedOperationException("This method is not implemented yet!");
-  }
-
-  public void send() {
-
-    // at some point in the process
-    //this.encode(/* some method, some data */);
-    
-    throw new UnsupportedOperationException("This method is not implemented yet!");
-  }
-
-  public void read() {
-
-    // at some point in the process
-    //this.encode(/* some method, some data */);
+    // what does buffer.size() return
 
     throw new UnsupportedOperationException("This method is not implemented yet!");
   }
 
+  public void send(String s) {
+    byte[] encodedInput = encode(s);
 
-  // There is an argument to suggest that this could include the encode/decode stuff as it is the
-  // only known usage of it as of yet. Probably a 'SOLID' discussion here. I really need to revise
-  // this.
+    p.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0);
+
+    // p.getOutputStream(); when connected to something will be for sending data out
+    comWriter = new PrintWriter(p.getOutputStream());
+
+    for (byte b : encodedInput) {
+      comWriter.print(b);
+    }
+
+    // cleanup
+    comWriter.flush();
+    comWriter.close();
+  }
+
+  public String read() {
+    p.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0);
+
+    // p.getInputStream(); when connected to something will get whatever is coming
+    // in
+    comReader = new Scanner(p.getInputStream());
+
+    while (comReader.hasNext()) {
+      buffer.add(comReader.nextByte());
+    }
+
+    //cleanup
+    comReader.close();
+
+    byte[] readData = new byte[buffer.size()];
+
+    for (int i = 0; i < readData.length; i++) {
+      readData[i] = (byte) buffer.get(i);
+      
+      //clear from buffer once byte has been moved
+      buffer.remove(i);
+    }
+
+    return decode(readData);
+  }
 
   // Will also for now be private as no need for outside access??
 
   /**
    * 
-   * --NOT SURE THIS WILL STAY A STRING BUT FOR THE BASIC IMPLEMENTATION--
-   * 
    * @param input The data to be encoded into Base64
    * @return The encoded version of the input as a string
    */
-  private String encode(String input) {
-    String encodedOutput = Base64.getEncoder().encodeToString(input.getBytes());
+  private byte[] encode(String input) {
+    byte[] encodedOutput = Base64.getEncoder().encode(input.getBytes());
     return encodedOutput;
   }
 
   /**
    * 
-   * @param input The incoming data to be 
+   * @param input The incoming data to be
    * @return
    */
   private String decode(byte[] input) {
     byte[] decodedBytes = Base64.getDecoder().decode(input);
     String decodedOutput = new String(decodedBytes);
     return decodedOutput;
+  }
+
+  // Good practice to cleanup behind you but unsure of exactly where it will slot in yet
+  public void closePipe() {
+    p.closePort();
   }
 }
