6604bdfc12299c5b57c107b6e55a4bfa8a25920b
Harry Jackson <h.jackson5@uni.brighton.ac.uk>
2018-03-28 14:20:05 +0100

    GUI Skeleton/ layout

--- mcomp_Java/src/member/ui/View.java
+++ mcomp_Java/src/member/ui/View.java
@@ -2,8 +2,15 @@ package member.ui;
 
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 
@@ -18,10 +25,38 @@ public class View extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
 		
-		StackPane root = new StackPane();
-		primaryStage.setScene(new Scene(root, 300, 250));
-		primaryStage.show();
+		Pane root = new Pane();
+		final Scene scene = new Scene(root ,500, 250);
+		primaryStage.setScene(scene);
+
 		
+		Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+		primaryStage.setX(screenBounds.getMinX());
+		primaryStage.setY(screenBounds.getMinY());
+		primaryStage.setWidth(screenBounds.getWidth());
+		primaryStage.setHeight(screenBounds.getHeight());
+		
+		Rectangle r = new Rectangle();
+		r.setX(screenBounds.getMinX()+10);
+		r.setY(screenBounds.getMinY());
+		r.setWidth(screenBounds.getWidth() / 1.5);
+		r.setHeight(screenBounds.getHeight() - 50);
+		r.setFill(Color.BLUE);
+		r.setStroke(Color.BLACK);
+		root.getChildren().add(r);
+		
+		Rectangle r2 = new Rectangle();
+		r2.setX(screenBounds.getMaxX() -405);
+		r2.setY(screenBounds.getMinY());
+		r2.setWidth(screenBounds.getWidth() - r.getWidth() - 50);
+		r2.setHeight(screenBounds.getHeight() - 50);
+		r2.setFill(Color.RED);
+		r2.setStroke(Color.BLACK);
+		root.getChildren().add(r2);
+		primaryStage.show();
 	}
+
+
+	
 
 }
