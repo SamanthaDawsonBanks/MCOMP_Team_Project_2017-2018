3206a709dd358ed39b2ba4e05a37822145f2e494
David Avery <D.Avery1@uni.brighton.ac.uk>
2018-04-04 20:15:45 +0100

    added BlockedUnblockedAmalgamated

--- mcomp_Java/src/unitTesting/testData/TestData.java
+++ mcomp_Java/src/unitTesting/testData/TestData.java
@@ -137,5 +137,38 @@ public class TestData {
 
   }
 
+  public static ArrayList<Waypoint> getBlockedUnblockedAmalgamated() {
+
+    ArrayList<int[]> ys = new ArrayList<int[]>();
+    ys.add(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20});
+    ys.add(new int[] { 1,             6,    8,                                 20});
+    ys.add(new int[] { 1, 2,                8, 9,10,11,12,13,      16,         20});
+    ys.add(new int[] { 1,                7, 8, 9,      12,         16,         20});
+    ys.add(new int[] { 1,          5, 6, 7,         11,         15,   17,      20});
+    ys.add(new int[] { 1, 2,       5, 6, 7,      10,            15,   17,      20});
+    ys.add(new int[] { 1, 2,    4, 5,          9,10,11,12,   14,   16,         20});
+    ys.add(new int[] { 1, 2,    4,          8,                     16,         20});
+    ys.add(new int[] { 1, 2,                              13,      16,         20});
+    ys.add(new int[] { 1, 2,                     10,11,12,      15,   17,      20});
+    ys.add(new int[] { 1, 2,             7, 8,               14,15,   17,      20});
+    ys.add(new int[] { 1, 2,          6, 7,               13,         17,      20});
+    ys.add(new int[] { 1, 2,    4, 5, 6,       9,10,11,12,13,   15,16,17,      20});
+    ys.add(new int[] { 1, 2,                                                   20});
+    ys.add(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20});
+    
+    ArrayList<Waypoint> l = new ArrayList<Waypoint>();
+    
+    int y = 1;
+    for (int[] line : ys) {
+      for (int x : line) {
+        l.add(new Waypoint(x,y));
+      }
+      y++;
+    }
+
+    return l;
+
+  }
+
 
 }
