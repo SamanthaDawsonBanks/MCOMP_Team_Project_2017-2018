b48583b84df6bc0e601852989f8c2045cf71d137
David Avery <D.Avery1@uni.brighton.ac.uk>
2018-04-04 19:36:44 +0100

    start of TestDataJ factor out

---
+++ mcomp_Java/src/unitTesting/testData/TestData.java
@@ -0,0 +1,110 @@
+/**
+ * 
+ */
+package unitTesting.testData;
+
+import java.util.ArrayList;
+import common.datatypes.Waypoint;
+
+/**
+ * @author David Avery 15823926
+ *
+ */
+public class TestData {
+
+  public static ArrayList<Waypoint> getPresentationMaze() {
+
+    ArrayList<int[]> ys = new ArrayList<int[]>();
+    ys.add(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15}); 
+    ys.add(new int[] { 1,                                 13,   15}); 
+    ys.add(new int[] { 1,    3, 4, 5, 6, 7, 8, 9,10,11,   13,   15}); 
+    ys.add(new int[] { 1,    3,          7,         11,   13,   15}); 
+    ys.add(new int[] { 1,    3,    5,    7,    9,   11,   13,   15}); 
+    ys.add(new int[] { 1,    3,    5,          9,   11,   13,   15}); 
+    ys.add(new int[] { 1,    3,    5, 6, 7, 8, 9,   11,12,13,   15}); 
+    ys.add(new int[] { 1,                      9,               15}); 
+    ys.add(new int[] { 1,    3, 4, 5, 6, 7,    9,10,11,12,13,   15}); 
+    ys.add(new int[] { 1,                           11,         15}); 
+    ys.add(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15}); 
+    
+    ArrayList<Waypoint> l = new ArrayList<Waypoint>();
+    
+    int y = 1;
+    for (int[] line : ys) {
+      for (int x : line) {
+        l.add(new Waypoint(x,y));
+      }
+      y++;
+    }
+
+    return l;
+
+  }
+
+  public static ArrayList<Waypoint> getEmptyMaze() {
+
+    ArrayList<int[]> ys = new ArrayList<int[]>();
+    ys.add(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15}); 
+    ys.add(new int[] { 1,                                       15}); 
+    ys.add(new int[] { 1,                                       15}); 
+    ys.add(new int[] { 1,                                       15}); 
+    ys.add(new int[] { 1,                                       15}); 
+    ys.add(new int[] { 1,                                       15}); 
+    ys.add(new int[] { 1,                                       15}); 
+    ys.add(new int[] { 1,                                       15}); 
+    ys.add(new int[] { 1,                                       15}); 
+    ys.add(new int[] { 1,                                       15}); 
+    ys.add(new int[] { 1,                                       15}); 
+    ys.add(new int[] { 1,                                       15}); 
+    ys.add(new int[] { 1,                                       15}); 
+    ys.add(new int[] { 1,                                       15}); 
+    ys.add(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15}); 
+    
+    ArrayList<Waypoint> l = new ArrayList<Waypoint>();
+    
+    int y = 1;
+    for (int[] line : ys) {
+      for (int x : line) {
+        l.add(new Waypoint(x,y));
+      }
+      y++;
+    }
+
+    return l;
+
+  }
+
+  public static ArrayList<Waypoint> getSteppedMaze() {
+
+    ArrayList<int[]> ys = new ArrayList<int[]>();
+    ys.add(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15}); 
+    ys.add(new int[] { 1,                                       15}); 
+    ys.add(new int[] { 1,                                 13,   15}); 
+    ys.add(new int[] { 1,                              12,      15}); 
+    ys.add(new int[] { 1,                           11,         15}); 
+    ys.add(new int[] { 1,                        10,            15}); 
+    ys.add(new int[] { 1,                      9,               15}); 
+    ys.add(new int[] { 1,                   8,                  15}); 
+    ys.add(new int[] { 1,                7,                     15}); 
+    ys.add(new int[] { 1,             6,                        15}); 
+    ys.add(new int[] { 1,          5,                           15}); 
+    ys.add(new int[] { 1,       4,                              15}); 
+    ys.add(new int[] { 1,    3,                                 15}); 
+    ys.add(new int[] { 1,                                       15}); 
+    ys.add(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15}); 
+    
+    ArrayList<Waypoint> l = new ArrayList<Waypoint>();
+    
+    int y = 1;
+    for (int[] line : ys) {
+      for (int x : line) {
+        l.add(new Waypoint(x,y));
+      }
+      y++;
+    }
+
+    return l;
+
+  }
+
+}
