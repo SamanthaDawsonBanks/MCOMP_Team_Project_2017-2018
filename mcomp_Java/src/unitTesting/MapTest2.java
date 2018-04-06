59d677fe787965faf6d77efc714a017c1c15a14a
DavidAveryUoB <d.avery1@uni.brighton.ac.uk>
2018-04-06 13:40:09 +0100

    Testing data j (#165)
    * start of TestDataJ factor out
    
    * refactored to use the new test data file
    
    * added maze2RealTestHeuristicPathOptimisation
    
    * added BlockedUnblockedAmalgamated
    
    * bugfix

--- mcomp_Java/src/unitTesting/MapTest2.java
+++ mcomp_Java/src/unitTesting/MapTest2.java
@@ -14,10 +14,11 @@ import common.datatypes.map.griddedMap.Vertex;
 import common.datatypes.path.Path;
 import pathfinding.AStar;
 import pathfinding.SearchMap;
+import unitTesting.testData.TestData;
 
 
 /**
- * @author David Avery
+ * @author Harry Jackson 14812630
  *
  */
 class MapTest2 {
@@ -37,7 +38,7 @@ class MapTest2 {
     ArrayList<Waypoint> l = new ArrayList<Waypoint>();
     l.add((new Waypoint(-1,-1)));
     Map n = new Map(64, new MapLayer(l));
-    Map m = new Map(64, new MapLayer(getPresentationMaze())); //TODO expand to full test
+    Map m = new Map(64, new MapLayer(TestData.getPresentationMaze())); //TODO expand to full test
 
     System.out.printf("");// breakpoint
 
@@ -54,200 +55,4 @@ class MapTest2 {
    // b.search(start, dest, m);
   }
    
- 
-
-  private ArrayList<Waypoint> getPresentationMaze() {
-    int y;
-    ArrayList<Waypoint> l = new ArrayList<Waypoint>();
-    
-    y = 1;
-    l.add(new Waypoint(1, y));
-    l.add(new Waypoint(2, y));
-    l.add(new Waypoint(3, y));
-    l.add(new Waypoint(4, y));
-    l.add(new Waypoint(5, y));
-    l.add(new Waypoint(6, y));
-    l.add(new Waypoint(7, y));
-    l.add(new Waypoint(8, y));
-    l.add(new Waypoint(9, y));
-    l.add(new Waypoint(10, y));
-    l.add(new Waypoint(11, y));
-    l.add(new Waypoint(12, y));
-    l.add(new Waypoint(13, y));
-    l.add(new Waypoint(14, y));
-    l.add(new Waypoint(15, y));
-    
-    y = 2;
-    l.add(new Waypoint(1, y));
-    // r.add(new Waypoint(2, y));
-    // r.add(new Waypoint(3, y));
-    // r.add(new Waypoint(4, y));
-    // r.add(new Waypoint(5, y));
-    // r.add(new Waypoint(6, y));
-    // r.add(new Waypoint(7, y));
-    // r.add(new Waypoint(8, y));
-    // r.add(new Waypoint(9, y));
-    // r.add(new Waypoint(10, y));
-    l.add(new Waypoint(11, y));
-    // r.add(new Waypoint(12, y));
-    // r.add(new Waypoint(13, y));
-    // r.add(new Waypoint(14, y));
-    l.add(new Waypoint(15, y));
-    
-    y = 3;
-    l.add(new Waypoint(1, y));
-    // r.add(new Waypoint(2, y));
-    l.add(new Waypoint(3, y));
-    l.add(new Waypoint(4, y));
-    l.add(new Waypoint(5, y));
-    l.add(new Waypoint(6, y));
-    l.add(new Waypoint(7, y));
-    // r.add(new Waypoint(8, y));
-    l.add(new Waypoint(9, y));
-    l.add(new Waypoint(10, y));
-    l.add(new Waypoint(11, y));
-    l.add(new Waypoint(12, y));
-    l.add(new Waypoint(13, y));
-    // r.add(new Waypoint(14, y));
-    l.add(new Waypoint(15, y));
-   
-    y = 4;
-    l.add(new Waypoint(1, y));
-    // r.add(new Waypoint(2, y));
-    // r.add(new Waypoint(3, y));
-    // r.add(new Waypoint(4, y));
-    // r.add(new Waypoint(5, y));
-    // r.add(new Waypoint(6, y));
-    // r.add(new Waypoint(7, y));
-    // r.add(new Waypoint(8, y));
-    l.add(new Waypoint(9, y));
-    // r.add(new Waypoint(10, y));
-    // r.add(new Waypoint(11, y));
-    // r.add(new Waypoint(12, y));
-    // r.add(new Waypoint(13, y));
-    // r.add(new Waypoint(14, y));
-    l.add(new Waypoint(15, y));
-    
-    y = 5;
-    l.add(new Waypoint(1, y));
-    // r.add(new Waypoint(2, y));
-    l.add(new Waypoint(3, y));
-    // r.add(new Waypoint(4, y));
-    l.add(new Waypoint(5, y));
-    l.add(new Waypoint(6, y));
-    l.add(new Waypoint(7, y));
-    l.add(new Waypoint(8, y));
-    l.add(new Waypoint(9, y));
-    // r.add(new Waypoint(10, y));
-    l.add(new Waypoint(11, y));
-    l.add(new Waypoint(12, y));
-    l.add(new Waypoint(13, y));
-    // r.add(new Waypoint(14, y));
-    l.add(new Waypoint(15, y));
-    
-    y = 6;
-    l.add(new Waypoint(1, y));
-    // r.add(new Waypoint(2, y));
-    l.add(new Waypoint(3, y));
-    // r.add(new Waypoint(4, y));
-    l.add(new Waypoint(5, y));
-    // r.add(new Waypoint(6, y));
-    // r.add(new Waypoint(7, y));
-    // r.add(new Waypoint(8, y));
-    l.add(new Waypoint(9, y));
-    // r.add(new Waypoint(10, y));
-    l.add(new Waypoint(11, y));
-    // r.add(new Waypoint(12, y));
-    l.add(new Waypoint(13, y));
-    // r.add(new Waypoint(14, y));
-    l.add(new Waypoint(15, y));
-   
-    y = 7;
-    l.add(new Waypoint(1, y));
-    // r.add(new Waypoint(2, y));
-    l.add(new Waypoint(3, y));
-    l.add(new Waypoint(4, y));
-    l.add(new Waypoint(5, y));
-    // r.add(new Waypoint(6, y));
-    l.add(new Waypoint(7, y));
-    // r.add(new Waypoint(8, y));
-    l.add(new Waypoint(9, y));
-    // r.add(new Waypoint(10, y));
-    l.add(new Waypoint(11, y));
-    // r.add(new Waypoint(12, y));
-    l.add(new Waypoint(13, y));
-    // r.add(new Waypoint(14, y));
-    l.add(new Waypoint(15, y));
-    
-    y = 8;
-    l.add(new Waypoint(1, y));
-    // r.add(new Waypoint(2, y));
-    // r.add(new Waypoint(3, y));
-    // r.add(new Waypoint(4, y));
-    // r.add(new Waypoint(5, y));
-    // r.add(new Waypoint(6, y));
-    l.add(new Waypoint(7, y));
-    // r.add(new Waypoint(8, y));
-    // r.add(new Waypoint(9, y));
-    // r.add(new Waypoint(10, y));
-    l.add(new Waypoint(11, y));
-    // r.add(new Waypoint(12, y));
-    l.add(new Waypoint(13, y));
-    // r.add(new Waypoint(14, y));
-    l.add(new Waypoint(15, y));
-    
-    y = 9;
-    l.add(new Waypoint(1, y));
-    // r.add(new Waypoint(2, y));
-    l.add(new Waypoint(3, y));
-    l.add(new Waypoint(4, y));
-    l.add(new Waypoint(5, y));
-    l.add(new Waypoint(6, y));
-    l.add(new Waypoint(7, y));
-    l.add(new Waypoint(8, y));
-    l.add(new Waypoint(9, y));
-    l.add(new Waypoint(10, y));
-    l.add(new Waypoint(11, y));
-    // r.add(new Waypoint(12, y));
-    l.add(new Waypoint(13, y));
-    // r.add(new Waypoint(14, y));
-    l.add(new Waypoint(15, y));
-    
-    y = 10;
-    l.add(new Waypoint(1, y));
-    // r.add(new Waypoint(2, y));
-    // r.add(new Waypoint(3, y));
-    // r.add(new Waypoint(4, y));
-    // r.add(new Waypoint(5, y));
-    // r.add(new Waypoint(6, y));
-    // r.add(new Waypoint(7, y));
-    // r.add(new Waypoint(8, y));
-    // r.add(new Waypoint(9, y));
-    // r.add(new Waypoint(10, y));
-    // r.add(new Waypoint(11, y));
-    // r.add(new Waypoint(12, y));
-    l.add(new Waypoint(13, y));
-    // r.add(new Waypoint(14, y));
-    l.add(new Waypoint(15, y));    
-    
-    y = 11;
-    l.add(new Waypoint(1, y));
-    l.add(new Waypoint(2, y));
-    l.add(new Waypoint(3, y));
-    l.add(new Waypoint(4, y));
-    l.add(new Waypoint(5, y));
-    l.add(new Waypoint(6, y));
-    l.add(new Waypoint(7, y));
-    l.add(new Waypoint(8, y));
-    l.add(new Waypoint(9, y));
-    l.add(new Waypoint(10, y));
-    l.add(new Waypoint(11, y));
-    l.add(new Waypoint(12, y));
-    l.add(new Waypoint(13, y));
-    l.add(new Waypoint(14, y));
-    l.add(new Waypoint(15, y));
-    return l;
-
-  }
-
-}
+ }
