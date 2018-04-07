4d52c32d82d7087fe5de241e1d5106d9914ba094
David Avery <D.Avery1@uni.brighton.ac.uk>
2018-04-07 16:26:17 +0100

    GridDesign docs

--- mcomp_Java/src/common/datatypes/map/griddedMap/GridDesign.java
+++ mcomp_Java/src/common/datatypes/map/griddedMap/GridDesign.java
@@ -1,38 +1,54 @@
-/**
- * 
- */
 package common.datatypes.map.griddedMap;
 
 /**
+ * Enum fo Grid designs (Expand later for shapes and dimensions)
+ * 
  * @author David Avery 15823926
+ * @version 1.0
+ * @since 2018-04-07
+ * 
+ * @see GridedMap
  *
  */
-  // FIXME for better enum use (sides and array of relatives?)
+public enum GridDesign {
+  /** Four sided (Regular Square) based tessellating grid */
+  TETRA;
+
+
   /**
-   * Grid designs (Expand later for shapes and dimensions)
+   * Access method for the grid design neighbour addresses
+   * 
+   * @see Vertex
+   *
+   * @return Array (size based on design) holding the relative addresses of the neighbours
+   * 
    */
-  public enum GridDesign {
-    /** Four sided (Regular Square) based tessellating grid */
-    TETRA;
+  RelativeAddress[] getNeighbourAddresses() {
+    switch (this) {
+      case TETRA:
+        RelativeAddress[] res = {new RelativeAddress(0, 1), new RelativeAddress(1, 0),
+            new RelativeAddress(0, -1), new RelativeAddress(-1, 0)};
+        return res;
+      default:
+        throw new AssertionError("Unknown design " + this);
+    }
+  };
 
-    RelativeAddress[] getNeighbourAddresses() {
-      switch (this) {
-        case TETRA:
-          RelativeAddress[] res = {new RelativeAddress(0, 1), new RelativeAddress(1, 0),
-              new RelativeAddress(0, -1), new RelativeAddress(-1, 0)};
-          return res;
-        default:
-          throw new AssertionError("Unknown design " + this);
-      }
-    };
-
-    public int getShapeSides() {//TODO expand from mixed grids (Octa with Tetra)
-      switch (this) {
-        case TETRA:
-          return 4;
-        default:
-          throw new AssertionError("Unknown design " + this);
-      }
+  /**
+   * Access method for the grid design neighbours
+   * 
+   * @see Vertex
+   *
+   * @return Number of edges that each Vertex of this type has
+   * 
+   */
+  public int getShapeSides() {// TODO expand from mixed grids (Octa with Tetra) array of sides?
+    switch (this) {
+      case TETRA:
+        return 4;
+      default:
+        throw new AssertionError("Unknown design " + this);
     }
   }
+}
 
