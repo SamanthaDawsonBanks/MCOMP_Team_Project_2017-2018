91c79dc9280d580e6e91beb9e1546a0ad8474d21
David Avery <D.Avery1@uni.brighton.ac.uk>
2018-04-08 11:06:08 +0100

    bossable docs

--- mcomp_Java/src/common/interfaces/Bossable.java
+++ mcomp_Java/src/common/interfaces/Bossable.java
@@ -6,24 +6,58 @@ import common.datatypes.path.Path;
 import common.objects.Herd;
 
 /**
+ * Bossable interface
+ * 
+ * A bossable object is one that can receive instructions from the Leader of a Herd to process a
+ * section of a particular item, either a segment of a Map to be augmented or a Path to be analysed.
+ * 
  * @author Stephen Pope 15836791
  * @author David Avery 15823926
  * @author Ryan Shoobert 15812407
  * @author Harry Jackson 14812630
+ * @author David Avery 15823926
  * 
- *         Bossable interface
+ * @version 1.0
+ * @since 2018-04-07
  * 
- *         A bossable object is one that can receive instructions from the Leader of a Herd to
- *         process a section of a particular item, either a segment of a Map to be augmented or a
- *         Path to be analysed.
- * 
+ * @see RemoteMember
+ * @see Member
+ * @see Herd
+ * @see Leader
+ * @see RemoteLeader
+ *
  */
 public interface Bossable {
 
-  public Map processMapLump(Herd h) throws RemoteException;// herd is the data holder for members,
-                                                           // maps, and paths
+  /**
+   * Processes a section of the map for amalgamation as part of a distributed call to the members
+   * 
+   * @see GriddedMap
+   * @see MapLayer
+   * @see Herd
+   * @see Leader
+   *
+   * @param h A Herd object holding the map data to be processed
+   * 
+   * @return The section of the map processed
+   * 
+   * @throws RemoteException
+   */
+  public Map processMapLump(Herd h) throws RemoteException;
 
-  public Path processPathLump(Herd h) throws RemoteException;// herd is the data holder for members,
-                                                             // maps, and paths
+  /**
+   * Processes a section of the path for pathfinding as part of a distributed call to the members
+   * 
+   * @see AStar //TODO adjust for refactored names
+   * @see Herd
+   * @see Leader
+   *
+   * @param h A Herd object holding the path data to be processed
+   * 
+   * @return The section of the path processed
+   * 
+   * @throws RemoteException
+   */
+  public Path processPathLump(Herd h) throws RemoteException;
 
 }
