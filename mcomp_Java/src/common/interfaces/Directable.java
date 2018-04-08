38ab5d53bce1d1b90f7bd6fe70553873482d8208
David Avery <D.Avery1@uni.brighton.ac.uk>
2018-04-08 12:06:06 +0100

    directable docs

--- mcomp_Java/src/common/interfaces/Directable.java
+++ mcomp_Java/src/common/interfaces/Directable.java
@@ -2,21 +2,42 @@ package common.interfaces;
 
 import java.rmi.RemoteException;
 import common.datatypes.Waypoint;
+import common.objects.Herd;
 
 /**
+ * A directable object is one that can accept input that defines a final goal or destination for the
+ * object to attempt to pathfind to.
+ * 
  * @author Stephen Pope 15836791
  * @author David Avery 15823926
  * @author Ryan Shoobert 15812407
  * @author Harry Jackson 14812630
+ * @author David Avery 15823926
  * 
- *         Directable interface
+ * @version 1.0
+ * @since 2018-04-07
  * 
- *         A directable object is one that can accept input that defines a final goal or destination
- *         for the Herd to attempt to pathfind to.
+ * @see RemoteMember
+ * @see Member
+ * @see Herd
+ * @see Leader
+ * @see RemoteLeader
+ *
  */
 
 public interface Directable {
-
+  /**
+   * Takes destination input and passes, or stores it
+   * 
+   * Note: Both RemoteLeader and RemoteMember implement this interface with Member passing the data
+   * from the control to the leader and leader taking it and recording it in the herd
+   * 
+   * @param w The Waypoint (x/y) tuple holding the destination as coordinates
+   * 
+   * @return Boolean True for successfully set, False for an error / rejection
+   * 
+   * @throws RemoteException
+   */
   public boolean setDestination(Waypoint w) throws RemoteException;
 
 }
