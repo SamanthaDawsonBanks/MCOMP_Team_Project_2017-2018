a7b4ae55adc68a74f5e141a292bafb25fe13dd29
David Avery <D.Avery1@uni.brighton.ac.uk>
2018-04-08 18:32:15 +0100

    updateable docs

--- mcomp_Java/src/common/interfaces/Updateable.java
+++ mcomp_Java/src/common/interfaces/Updateable.java
@@ -4,25 +4,50 @@ import java.rmi.RemoteException;
 import common.objects.Herd;
 
 /**
+ * An object is updateable if it represents the 'model' / data storage holder object (Leader) in the
+ * MVC pattern.
+ * 
  * @author Stephen Pope 15836791
  * @author David Avery 15823926
  * @author Ryan Shoobert 15812407
  * @author Harry Jackson 14812630
+ * 
+ * @version 1.0
+ * @since 2018-04-07
+ * 
+ * @see common.interfaces.RemoteMember
+ * @see common.objects.Member
+ * @see common.objects.Herd
+ * @see common.objects.Leader
+ * @see common.interfaces.RemoteLeader
  *
- *         Updateable interface
- * 
- *         An object is updateable if it represents the model in the MVC pattern.
- * 
  */
-
 public interface Updateable {
 
-  public void updateModel(Herd newHerdData) throws RemoteException; // FIXME this probably takes
-                                                                    // some info
+  /**
+   * Update method for setting the current state of the model
+   * 
+   * @see common.objects.Herd
+   * @see common.objects.Leader
+   * 
+   * @param Herd object containing the Member/Ability collections, Map, Destination, and Path with new info
+   * 
+   * @throws RemoteException RMI between Member-Leader
+   */
+  public void updateModel(Herd newHerdData) throws RemoteException;
 
-  // there will be many methods that will do the updates
-  // e.g robot is now at this location
-
-  public Herd getState() throws RemoteException; // returns the current state
-
+  /**
+   * Access method for getting the current state of the model typically called in response to a
+   * notify call
+   * 
+   * @see common.objects.Herd
+   * @see common.objects.Leader
+   * @see common.interfaces.Notifyable
+   * @see common.datatypes.Ability
+   * 
+   * @return Herd object containing the Member/Ability collections, Map, Destination, and Path
+   * 
+   * @throws RemoteException RMI between Member-Leader
+   */
+  public Herd getState() throws RemoteException;
 }
