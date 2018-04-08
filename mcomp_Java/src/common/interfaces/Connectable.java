81cd19624be3b022545bd07c92710e4536361b69
David Avery <D.Avery1@uni.brighton.ac.uk>
2018-04-08 11:09:37 +0100

    connectable docs

--- mcomp_Java/src/common/interfaces/Connectable.java
+++ mcomp_Java/src/common/interfaces/Connectable.java
@@ -2,19 +2,27 @@ package common.interfaces;
 
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import common.objects.Herd;
 
 /**
+ * An object is connectable if it assumes the role of an RMI server, meaning it can accept
+ * connections from multiple clients who wish to register their methods with the RMI registry.
+ * 
  * @author Stephen Pope 15836791
  * @author David Avery 15823926
  * @author Ryan Shoobert 15812407
  * @author Harry Jackson 14812630
+ * @author David Avery 15823926
  * 
- *         Connectable interface
+ * @version 1.0
+ * @since 2018-04-07
  * 
- *         An object is connectable if it assumes the role of an RMI server, meaning it can accept
- *         connections from multiple clients who wish to register their methods with the RMI
- *         registry.
- * 
+ * @see RemoteMember
+ * @see Member
+ * @see Herd
+ * @see Leader
+ * @see RemoteLeader
+ *
  */
 
 public interface Connectable {
@@ -24,7 +32,9 @@ public interface Connectable {
    * Receive instructions from it.
    * 
    * @param joiningMember The member of the herd that is registering itself with the leader
-   * @return Returns true if the member was registered successfully, False if it did not
+   * 
+   * @return The members group plus the leaving member.
+   * 
    * @throws RemoteException
    */
   public ArrayList<RemoteMember> register(RemoteMember joiningMember) throws RemoteException;
@@ -35,7 +45,9 @@ public interface Connectable {
    * 
    * @param leavingMember The member of the herd that is leaving and this unregistering with the
    *        leader.
-   * @return Returns true if the member unregistered with the leader, False if it did not
+   *        
+   * @return The members group minus the leaving member.
+   * 
    * @throws RemoteException
    */
   public ArrayList<RemoteMember> deregister(RemoteMember leavingMember) throws RemoteException;
