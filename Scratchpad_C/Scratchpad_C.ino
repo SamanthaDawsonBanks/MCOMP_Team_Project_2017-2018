9acbc4228e5efcae74183fe77ae246a64c61e578
Stephen Pope <stevempope@gmail.com>
2018-04-10 16:12:06 +0100

    Print LiDAR packets based on header to console

--- Scratchpad_C/Scratchpad_C.ino
+++ Scratchpad_C/Scratchpad_C.ino
@@ -3,7 +3,7 @@
 #define CONSOLE Serial //usb to console
 #define CHANNEL Serial1 //board to board TX1 RX1 on Mega 2560
 byte inByte;
-String buffer;
+byte buffer [22];
 
 //The setup function is called once at startup of the sketch
 void setup()
@@ -12,16 +12,29 @@ void setup()
   CHANNEL.begin(115200);
 }
 
+unsigned int packetToNumber(String packet){
+  unsigned int res;
+  res = (packet.charAt(0));
+
+  return res;
+}
+
 void loop()
 {
   if (CHANNEL.available()) {
     inByte = CHANNEL.read();
     if(inByte == 0xFA){
-      buffer = 0xFA;
-      for(int i = 0; i < 22; i++){
-        buffer += CHANNEL.read();
+      buffer [0] = 0xFA;
+      CONSOLE.println (0xFA);
+      CONSOLE.println("Start Packet");
+      for(int i = 1; i < 22; i++){
+        buffer [i] = CHANNEL.read();
+        CONSOLE.print(buffer[i]);
+        CONSOLE.println(" ");
       }
-      CONSOLE.println(buffer);
+    //  CONSOLE.print(buffer[0]);
+    //  CONSOLE.print(" : ");
+    //  CONSOLE.println(buffer[1]);
     }
   }
    // CONSOLE.println(CHANNEL.read(),HEX);
