ec291e3ccc3cc11fb829b5d1ae107e122cd55223
Stephen Pope <stevempope@gmail.com>
2018-04-10 15:33:38 +0100

    Added reading single packets at a time from LiDAR Sensor

--- Scratchpad_C/Scratchpad_C.ino
+++ Scratchpad_C/Scratchpad_C.ino
@@ -1,12 +1,36 @@
 #include "Arduino.h"
+
+#define CONSOLE Serial //usb to console
+#define CHANNEL Serial1 //board to board TX1 RX1 on Mega 2560
+byte inByte;
+String buffer;
+
 //The setup function is called once at startup of the sketch
 void setup()
 {
-// Add your initialization code here
+  CONSOLE.begin(115200);
+  CHANNEL.begin(115200);
 }
 
-// The loop function is called in an endless loop
 void loop()
 {
-//Add your repeated code here
+  if (CHANNEL.available()) {
+    inByte = CHANNEL.read();
+    if(inByte == 0xFA){
+      buffer = 0xFA;
+      for(int i = 0; i < 22; i++){
+        buffer += CHANNEL.read();
+      }
+      CONSOLE.println(buffer);
+    }
+  }
+   // CONSOLE.println(CHANNEL.read(),HEX);
+   // CONSOLE.write(CHANNEL.read()); //if there is something in the channel buffer read it, print it to the console, and loop
+  //}
+
+  //if (CONSOLE.available()) {
+  //  CHANNEL.write(CONSOLE.read()); //if there is something in the console buffer read it, send it to the channel, and loop
+  //}
 }
+
+
