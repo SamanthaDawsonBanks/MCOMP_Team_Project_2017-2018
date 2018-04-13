8b8f041be2fbd06ac3a7b0ba6a25a1c51cb2ae31
RyanShoobertUoB <shoobertr@gmail.com>
2018-04-13 13:16:29 +0100

    Initial Files

---
+++ Scratchpad_C/Release/sloeber.ino.cpp.d
@@ -0,0 +1,37 @@
+sloeber.ino.cpp.o: ..\sloeber.ino.cpp \
+ G:\PortableApps\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/Arduino.h \
+ G:\PortableApps\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/binary.h \
+ G:\PortableApps\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/WCharacter.h \
+ G:\PortableApps\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/WString.h \
+ G:\PortableApps\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/HardwareSerial.h \
+ G:\PortableApps\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/Stream.h \
+ G:\PortableApps\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/Print.h \
+ G:\PortableApps\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/Printable.h \
+ G:\PortableApps\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/USBAPI.h \
+ G:\PortableApps\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/Arduino.h \
+ G:\PortableApps\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\variants\mega/pins_arduino.h \
+ ..\Scratchpad_C.ino
+
+G:\PortableApps\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/Arduino.h:
+
+G:\PortableApps\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/binary.h:
+
+G:\PortableApps\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/WCharacter.h:
+
+G:\PortableApps\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/WString.h:
+
+G:\PortableApps\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/HardwareSerial.h:
+
+G:\PortableApps\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/Stream.h:
+
+G:\PortableApps\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/Print.h:
+
+G:\PortableApps\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/Printable.h:
+
+G:\PortableApps\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/USBAPI.h:
+
+G:\PortableApps\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/Arduino.h:
+
+G:\PortableApps\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\variants\mega/pins_arduino.h:
+
+..\Scratchpad_C.ino:
