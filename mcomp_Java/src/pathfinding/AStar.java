f5ffeca53aa173dbf2895eaaa29c4fd81e0e0efa
David Avery <D.Avery1@uni.brighton.ac.uk>
2018-04-17 14:55:33 +0100

    fixed AStar for SV

--- mcomp_Java/src/pathfinding/AStar.java
+++ mcomp_Java/src/pathfinding/AStar.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import common.datatypes.Waypoint;
 import common.datatypes.map.Map;
+import common.datatypes.map.griddedMap.BlockedVertex;
 import common.datatypes.map.griddedMap.Vertex;
 import common.datatypes.path.Path;
 
@@ -15,36 +16,29 @@ public class AStar {
   private ArrayList<ScoredVertex> openList = new ArrayList<ScoredVertex>();
   private ArrayList<ScoredVertex> closedList = new ArrayList<ScoredVertex>();
 
-  public void pathfind(Waypoint start, Waypoint dest, Map m) {
+  public Path pathfind(Waypoint start, Waypoint dest, Map m) {
 
-    PathOptimisation p = new PathOptimisation();
+    PathOptimisation pOpt = new PathOptimisation();
 
-    ScoredVertex svStart = new ScoredVertex(m.getAmalgamatedMap().getVertex(dest), m.getAmalgamatedMap());
-    ScoredVertex svDest = new ScoredVertex(m.getAmalgamatedMap().getVertex(start), m.getAmalgamatedMap());
+    ScoredVertex svStart =
+        new ScoredVertex(m.getAmalgamatedMap().getVertex(dest), m.getAmalgamatedMap());
+    ScoredVertex svDest =
+        new ScoredVertex(m.getAmalgamatedMap().getVertex(start), m.getAmalgamatedMap());
+
+    BlockedVertex blocked = m.getAmalgamatedMap().blocked;
 
 
     svStart.setGx(0.0);
-    svStart.setHx(h.euclideanHeuristic(svStart, svDest));
-    svStart.setFx(svStart.getHx());
+    svStart.setHx(h.manhattanHeuristic(svStart, svDest));
 
     openList.add(svStart);
 
-    int counter = 0;
     while (!openList.isEmpty()) {
 
       ScoredVertex current = svStart;
 
-      if (openList.size() == 0) {
-        System.out.print("no route");
-        throw new RuntimeException("no route");
-      }
-
-      if (openList.contains(svStart)) {
-        svStart.setFx(new Heuristic().manhattanHeuristic(svStart, svDest) + svStart.getGx());
-      }
-
       for (ScoredVertex v : openList) {
-        if (current == null || v.getFx() < current.getFx()) {
+        if (v.getFx() < current.getFx()) {
           current = v;
         }
         if (v.getFx() == current.getFx()) {
@@ -57,75 +51,77 @@ public class AStar {
       if (isEquals(current, svDest)) {
         System.out.print("goal reached");
         System.out.print(closedList.size());
-        returnPath(svStart, svDest, m);
-        break;
+        return returnPath(svStart, current, m);
       }
 
 
       openList.remove(current);
       closedList.add(current);
-      System.out.println("current node: " + current.getX() + "," + current.getY());
-      counter++;
+      System.out.println("current node: " + current.getX() + "," + current.getY() + " (" + "fx: "
+          + current.getFx() + " hx: " + current.getHx() + " gx: " + current.getGx() + ") ");
+      System.out.println();
 
 
       for (Vertex v : current.edges) {
 
-        if (v == null) {
+        if (v == null || v.equals(blocked) || myContains(closedList, v)) {// FIXME null should be
+                                                                          // open?
           continue;
         }
 
         ScoredVertex z = new ScoredVertex(v, m.getAmalgamatedMap());
+        z.setHx(h.manhattanHeuristic(z, svDest));
 
-        z.setGx(current.getGx() + 1);
-        double childGx = z.getGx();
-
-        if (childGx < z.getGx()) {
-          openList.remove(z);
-          closedList.remove(z);
-        }
-
-        if (!myContains(openList, z) && !myContains(closedList, z)
-            && !v.equals(m.getAmalgamatedMap().blocked)) {
-          z.setGx(childGx);
-          z.setHx(h.manhattanHeuristic(z, svDest));
-          z.setFx(z.getHx() + z.getGx());
-          z.parent = current;
+        if (!myContains(openList, z)) {
           openList.add(z);
         }
-        if (v.equals(m.getAmalgamatedMap().blocked)) {
-          System.out.print("");
-        } else if (!closedList.contains(z)) {
+
+        double childGx = z.getGx() + 1;
+
+        if (childGx < z.getGx()) {
+          continue;
+        }
+
+        z.setGx(childGx);
+        z.parent = current;
+
+        if (!closedList.contains(z)) {
           System.out.printf("child:(%d,%d)" + " gx = %.1f" + " hx = %.1f" + " fx = %.1f" + "\n",
               z.getX(), z.getY(), z.getGx(), z.getHx(), z.getFx());
         }
       }
 
+      System.out.println();
       System.out.print("Open: ");
       for (ScoredVertex v : openList) {
-        System.out.print("(" + v.getX() + "," + v.getY() + " fx: " + v.getFx() + " hx: " + v.getHx()
-            + " gx: " + v.getGx() + ") ");
+        System.out.println("(" + v.getX() + "," + v.getY() + " fx: " + v.getFx() + " hx: "
+            + v.getHx() + " gx: " + v.getGx() + ") ");
       }
       System.out.println();
       System.out.print("Closed: ");
-      for (Vertex v : closedList) {
-        System.out.printf("(%d,%d) ", v.getX(), v.getY());
+      for (ScoredVertex v : closedList) {
+        System.out.println("(" + v.getX() + "," + v.getY() + " fx: " + v.getFx() + " hx: "
+            + v.getHx() + " gx: " + v.getGx() + ") ");
       }
       System.out.printf("\n \n");
     }
+
+    System.out.print("no route");
+    throw new RuntimeException("no route");
+
   }
+
 
 
   public Path returnPath(ScoredVertex start, ScoredVertex dest, Map m) {
     ScoredVertex current = dest;
 
     routeToStart.add(dest);
-
     while (!isEquals(current, start)) {
       current = (ScoredVertex) current.parent;
       routeToStart.add((ScoredVertex) current);
-
     }
-    routeToStart.add(start);
+
     System.out.print("\n" + "Path to take:");
     Collections.reverse(routeToStart);
 
@@ -144,17 +140,17 @@ public class AStar {
   public boolean isEquals(Vertex a, Vertex b) {
     return (a.getX() == b.getX() && a.getY() == b.getY());
   }
-  
 
-  public boolean myContains(ArrayList<ScoredVertex> l, ScoredVertex v) {
+
+  public boolean myContains(ArrayList<ScoredVertex> l, Vertex v) {
     boolean res = false;
-    for (ScoredVertex sv : l) {
+    for (Vertex sv : l) {
       if (isEquals(sv, v)) {
         res = true;
       }
     }
     return res;
   }
-  
-  
+
+
 }
