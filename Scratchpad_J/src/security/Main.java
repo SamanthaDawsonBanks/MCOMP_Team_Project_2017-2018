ad237bc0b309b882df02c4c89d848544fe91ce51
Stephen Pope <stevempope@gmail.com>
2018-04-18 11:21:50 +0100

    Fixed wrong folder issue

---
+++ Scratchpad_J/src/security/Main.java
@@ -0,0 +1,11 @@
+package security;
+public class Main {
+	public static void main(String args[]) {
+		Scrambler s = new Scrambler();
+		Key k = new Key();
+		String test = "Hello Brighton!";
+		String encrypted = Scrambler.encrypt(test, k.getKey());
+		System.out.print("Encrpyted String: " + encrypted);
+		System.out.print("Decrypted String: " + Scrambler.decrypt(encrypted, k.getKey()));
+	}
+}
